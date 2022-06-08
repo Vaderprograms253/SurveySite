1748ba27d361fa97931d4f6fe2edaea708b14c2a
Xavier Denson <Denson.Xavier@student.greenriver.edu>
2022-06-08 15:37:29 -0400

    changes to the controller and index

--- controller/Controller.php
+++ controller/Controller.php
@@ -2,5 +2,60 @@
 
 class Controller
 {
+    private $_f3;
+
+    /*
+     * Controller constructor
+     */
+    function __construct($f3)
+    {
+        $this->_f3 = $f3;
+    }
+
+    /*
+     *
+     */
+    function home()
+    {
+        if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+            $userName = $_POST['username'];
+            $password = $_POST['password'];
+
+            //check if user wants to sign up
+            if (isset($_POST['login'])){
+                $user = new User();
+                if (SurveyValidation::validUserName($userName)){
+                    $user->setUserName($userName);
+                } else {
+                    $this->_f3->set('errors[username]', "Please enter valid user name");
+                }
+
+                if (SurveyValidation::validPassword($password)){
+                    $user->setUserPassword($password);
+                } else {
+                    $this->_f3->set('errors[password]', "Please enter valid password");
+                }
+            }
+
+
+        }
+        $view = new Template();
+        echo $view->render('views/home.html');
+    }
+
+    function login(){
+        $view = new Template();
+        echo $view->render('views/login.php');
+    }
+
+    function loggedIn(){
+        $view = new Template();
+        echo $view->render('views/loggedIn.php');
+    }
+
+    function reviews(){
+        $view = new Template();
+        echo $view->render('views/reviews.html');
+    }
 
 }
--- index.php
+++ index.php
@@ -4,20 +4,24 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 require_once('vendor/autoload.php');
+require 'config.php';
 
 $f3 = Base::instance();
+$con = new Controller($f3);
 
 $f3->route('GET /', function () {
-    $view = new Template();
-    echo $view->render('views/home.html');
-
+    $GLOBALS['con']->home();
 });
+
+//route to login page from nav
+$f3->route('GET /login', function () {
+    $GLOBALS['con']->login();
+});
+
 
 //route to review page
 $f3->route('GET /review', function () {
-    $view = new Template();
-    echo $view->render('views/reviews.html');
-
+    $GLOBALS['con']->reviews();
 });
 
 //route to home page from nav
@@ -26,11 +30,6 @@ $f3->route('GET /home', function () {
     echo $view->render('views/home.html');
 });
 
-//route to login page from nav
-$f3->route('GET /login', function () {
-    $view = new Template();
-    echo $view->render('login.php');
-});
 
 //route to loggedIn page from nav
 $f3->route('GET /loggedIn', function () {
