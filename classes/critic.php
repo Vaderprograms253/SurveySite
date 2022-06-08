b8745619502566c54b7255a57ed5df208c119097
neagle-spec <neagleaaron@hotmail.com>
2022-06-08 15:34:10 -0400

    commented code

--- classes/critic.php
+++ classes/critic.php
@@ -1,25 +1,28 @@
 <?php
 /*
- *
+ *328/SurveySite/classes/critic.php
  */
 class Critic extends User
 {
+    //declare and initialize instance variables
     private $_comments = "";
 
     /**
-     * @return string
-     */
-    public function getComments(): string
-    {
-        return $this->_comments;
-    }
-
-    /**
+     * Set critic entered comments
      * @param string $comments
      */
     public function setComments(string $comments): void
     {
         $this->_comments = $comments;
+    }
+
+    /**
+     * Return critic entered comments
+     * @return string
+     */
+    public function getComments(): string
+    {
+        return $this->_comments;
     }
 
 
