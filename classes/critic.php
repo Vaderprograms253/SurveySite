c0aa85906c35116ff1260346d199109b1ce5a864
neagle-spec <neagleaaron@hotmail.com>
2022-06-08 15:15:32 -0400

    declared variables, added getters and setters

--- classes/critic.php
+++ classes/critic.php
@@ -4,5 +4,23 @@
  */
 class Critic extends User
 {
+    private $_comments = "";
+
+    /**
+     * @return string
+     */
+    public function getComments(): string
+    {
+        return $this->_comments;
+    }
+
+    /**
+     * @param string $comments
+     */
+    public function setComments(string $comments): void
+    {
+        $this->_comments = $comments;
+    }
+
 
 }
