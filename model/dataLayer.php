0f48bdbe38042923d579aa5fbb4a5cdd78490a77
neagle-spec <neagleaaron@hotmail.com>
2022-06-14 14:43:53 -0400

    created insert function to add user to database

--- model/dataLayer.php
+++ model/dataLayer.php
@@ -17,8 +17,39 @@ class DataLayer
     {
         //connect to database
         require_once $_SERVER['DOCUMENT_ROOT'].'/../config.php';
+
+        // Turn on PDO error reporting
         $this->_dbh = $dbh;
         $this->_dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $this->_dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    }
+
+    function insertMember($member)
+    {
+        var_dump($member);
+
+        //1. define the query
+        $sql = "INSERT INTO members(id, username, pass, rating)
+                VALUES(:id, :username, :pass, :rating)";
+
+        //2. prepare statement
+        $statement = $this->_dbh->prepare($sql);
+
+        //3. bind parameters
+        $username = $member->getUsername();
+        $pass = $member->getPass();
+        $rating = $member->getRating();
+
+        $statement->bindParam(':username', $username, PDO::PARAM_STR);
+        $statement->bindParam(':pass', $pass, PDO::PARAM_STR);
+        $statement->bindParam(':rating',$rating, PDO::PARAM_STR);
+
+        //4. Execute the prepared statement
+        $statement->execute();
+
+        //5. Process the result
+        $id = $this->_dbh->lastInsertId();
+        echo "Row inserted: $id";
+        return $id;
     }
 }
